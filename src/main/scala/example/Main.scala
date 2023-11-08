04daf23ba5416c0bbbef84095d4cc6e322c47b1d
Windymelt <windymelt@3qe.us>
2023-11-09 00:09:00 +0900

    can load css in scalajs

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -2,11 +2,20 @@ package example
 
 import org.scalajs.dom
 
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
 
-@main
-def helloWorld(): Unit =
-  dom.document.querySelector("#app").innerHTML = s"""
+object Main {
+  @JSImport("@find/**/partial.css")
+  @js.native
+  private def importStyle(): Unit = js.native
+  importStyle()
+
+  @main
+  def helloWorld(): Unit =
+    dom.document.querySelector("#app").innerHTML = s"""
     <div>
-      <h1>Hello World!</h1>
+      <h1>Hello <p class="emph">World!</p></h1>
     </div>
   """
+}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
+import importSideEffectPlugin from "@raquo/vite-plugin-import-side-effect";
+import globResolverPlugin from "@raquo/vite-plugin-glob-resolver";
 
 export default defineConfig({
-  plugins: [scalaJSPlugin()],
+    plugins: [scalaJSPlugin(),
+              globResolverPlugin({
+                  cwd: __dirname,
+                  ignore: [
+                      'node_modules/**',
+                      'target/**'
+                  ]
+              }),
+              importSideEffectPlugin({
+                  // See https://github.com/raquo/vite-plugin-import-side-effect
+                  defNames: ["importStyle"],
+                  rewriteModuleIds: ['**/*.css', '**/*.less'],
+                  verbose: true
+              }),
+             ],
 });
